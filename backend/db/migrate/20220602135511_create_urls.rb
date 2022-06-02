93ccf222cd5fece84e4330cf840bef5bb7c5587a
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-03 07:45:29 +0900

    short_urlの文字制限数を30から80に変更

--- backend/db/migrate/20220602135511_create_urls.rb
+++ backend/db/migrate/20220602135511_create_urls.rb
@@ -2,7 +2,7 @@ class CreateUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :urls do |t|
       t.string :original_url, null: false
-      t.string :short_url, null: false, limit: 30
+      t.string :short_url, null: false, limit: 80
       t.timestamps
     end
   end
