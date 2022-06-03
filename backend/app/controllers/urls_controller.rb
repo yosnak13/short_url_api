26ed89ead509b33299acca00043e40cd9c16d8d4
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-03 21:28:50 +0900

    create追加

--- backend/app/controllers/urls_controller.rb
+++ backend/app/controllers/urls_controller.rb
@@ -4,5 +4,17 @@ class UrlsController < ApplicationController
   end
 
   def create
+    url = Url.new(url_params)
+    if url.save
+      render json: url
+    else
+      render json:url.errors, status: 422
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:original_url, :short_url)
   end
 end
