3d698a347ae3bcba5d8ed58c7e81aa4d41252b69
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-03 21:25:53 +0900

    indexのルート作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/urls", to: "urls#index"
+  resources :register_url, only: [:index, :new, :create]
 end
