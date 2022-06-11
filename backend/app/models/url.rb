12f3c9e5d853971a03a07e20e0e887e86129149f
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-11 12:17:02 +0900

    short_urlのユニーク化解除

--- backend/app/models/url.rb
+++ backend/app/models/url.rb
@@ -3,7 +3,7 @@ class Url < ApplicationRecord
 
   with_options presence: true do
     validates :original_url
-    validates :short_url, length: { in: 1..80 }, uniqueness: true
+    validates :short_url, length: { in: 1..80 }
   end
 
   def create_short_url
