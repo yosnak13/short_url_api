c6b6593c820e6b362442fb0813655cbe1ffec627
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-11 12:08:56 +0900

    ランダムな文字列をshort_urlにする下準備

--- backend/app/models/url.rb
+++ backend/app/models/url.rb
@@ -1,6 +1,13 @@
 class Url < ApplicationRecord
+  require 'securerandom'
+
   with_options presence: true do
     validates :original_url
     validates :short_url, length: { in: 1..80 }
+  end
+
+  def create_short_url
+    short = SecureRandom.alphanumeric(5)
+    
   end
 end
