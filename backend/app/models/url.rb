fb730c2c62fc4df605a228bec439835409270174
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-11 12:51:14 +0900

    不要メソッド削除

--- backend/app/models/url.rb
+++ backend/app/models/url.rb
@@ -1,13 +1,6 @@
 class Url < ApplicationRecord
-  require 'securerandom'
-
   with_options presence: true do
     validates :original_url
     validates :short_url, length: { in: 1..80 }
   end
-
-  # def create_short_url
-  #   short = SecureRandom.alphanumeric(5)
-  #   url.update(short_url: "#{url.id}".to_s + short)
-  # end
 end
