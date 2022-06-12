88b95c8c0ccc1bab674d84fc6ff66ccea7248f5b
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-12 10:08:45 +0900

    短縮URL発行時にhttp://を付与しない設定に変更

--- backend/app/models/url.rb
+++ backend/app/models/url.rb
@@ -5,7 +5,7 @@ class Url < ApplicationRecord
   end
 
   def create_short_url(url)
-    short_string = "http://" + SecureRandom.alphanumeric(5).to_s
+    short_string = SecureRandom.alphanumeric(5).to_s
     url.update(short_url: short_string)
   end
 end
