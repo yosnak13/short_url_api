dfef09f09760b72e679ec5b927f0c6c75a277f90
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-19 07:46:23 +0900

    簡単なVueの練習

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+import Welcome from '../views/Welcome'
+// import Register_url from '../views/register-url'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ frontend/src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p v-if='isEnabled'>{{ subtitle }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      title: '初めてのVue.jsアプリです！',
+      subtitle: 'ようこそ',
+      isEnabled: true
+    }
+  }
+}
+</script>
