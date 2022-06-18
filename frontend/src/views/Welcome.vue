1bf13954dcf3c3739cbc38bff7f3b4e4aca7a062
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2022-06-19 08:07:55 +0900

    data, computed, methodの学習

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -2,15 +2,34 @@
   <div>
     <h1>{{ title }}</h1>
     <p v-if='isEnabled'>{{ subtitle }}</p>
+    <button @dblclick='toggle'>トグルする</button>
+    <p>{{ text }}</p>
   </div>
 </template>
 <script>
 export default {
   data(){
+    // データプロパティを記述
     return {
       title: '初めてのVue.jsアプリです！',
       subtitle: 'ようこそ',
       isEnabled: true
+    }
+  },
+  computed: {
+    // 算出プロパティ(計算機能がついているプロパティのこと)
+    text(){
+      if(this.isEnabled){
+        return 'こんにちは！'
+      } else {
+        return 'さようなら！'
+      }
+    }
+  },
+  methods: {
+    // JSのメソッドを記述
+    toggle(){
+      this.isEnabled = !this.isEnabled
     }
   }
 }
